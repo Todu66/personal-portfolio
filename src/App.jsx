93c47a4a3fd33bf71fcd8ef37799bdb99eb11b67
Giga Todua <89189706+Todu66@users.noreply.github.com>
2023-10-09 14:59:42 +0400

    installing git

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import { About } from "./components/About";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="max-w-[1640px] h-[100vh] bg-[#0F172A] p-2">
+      <About />
+    </div>
+  );
+}
+
+export default App;
