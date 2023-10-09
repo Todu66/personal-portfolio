e9bcbb484d30d72f942699232a8422099c3e5fed
Giga Todua <89189706+Todu66@users.noreply.github.com>
2023-10-09 23:35:22 +0400

    fixing height after added slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 
 import { About } from "./components/About";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 import "./index.css";
 
 function App() {
   return (
-    <div className="max-w-[1640px] h-[100vh] bg-[#0F172A] p-2">
+    <div className="max-w-[1640px]  bg-[#0F172A] p-2">
       <About />
     </div>
   );
