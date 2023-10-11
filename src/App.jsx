6f922ed7dc9faeb0b864710045c9153381960c87
Giga Todua <89189706+Todu66@users.noreply.github.com>
2023-10-11 15:35:53 +0400

    adding project section

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { About } from "./components/About";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 import "./index.css";
+import { Projects } from "./components/Projects";
 
 function App() {
   return (
     <div className="max-w-[1640px]  bg-[#0F172A] p-2">
       <About />
+      <Projects />
     </div>
   );
 }
