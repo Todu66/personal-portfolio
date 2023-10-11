5fae981cb604bf2429ee3ba695ed12df3b2d4b9a
Giga Todua <89189706+Todu66@users.noreply.github.com>
2023-10-11 15:37:13 +0400

    adding project section

---
+++ src/components/Projects.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import spotifyApp from "../components/assets/spotifyApp.png"
+export const Projects = () => {
+  return (
+    <div>
+        <div className='mt-8'>
+            <p className="text-[#E6E6E6] uppercase ">Projects</p>   
+
+        </div>
+
+        <div className='flex flex-col gap-3 flex-coltext-[#AAAAAA] font-medium'>
+            <div className='flex flex-row items-center gap-2 mt-4'>
+                
+
+                <p className=' text-[#E6E6E6]'>Spotify PlayList</p>
+                <a href='https://jamming-react-todu.surge.sh/' target='_blank'>
+                 <i className="fa-solid fa-arrow-up-right-from-square text-[#c7cdd6] text-[15px] cursor-pointer hover:text-[#64ffda] "></i>
+                </a>
+                
+    
+            </div>
+            <p className='text-[#AAAAAA]'>Discover the power of Spotify API integration with this ReactJS project. This application allows you to seamlessly log in to your Spotify account, search for music, and efficiently manage your playlists. Enjoy the convenience of adding and removing songs from your playlists, all within a user-friendly ReactJS environment. Embark on a musical journey like never before with this practical Spotify API project!</p>
+            <div className='w-50 border-solid border-2 border-[#B8B8B8] rounded'>
+                <a href='https://jamming-react-todu.surge.sh/' target='_blank'>
+                <img className='' src={spotifyApp}/>
+                </a>
+                
+            </div>
+        </div>
+    </div>
+  )
+}
