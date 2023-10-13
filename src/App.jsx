4a80e1882fca64aac47c65ef5955ba5046faf2e0
Giga Todua <89189706+Todu66@users.noreply.github.com>
2023-10-13 20:42:22 +0400

    adding header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import { About } from "./components/About";
+import { Header } from "./components/Header";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 import "./index.css";
@@ -8,6 +9,7 @@ import { Projects } from "./components/Projects";
 function App() {
   return (
     <div className="max-w-[1640px]  bg-[#0F172A] p-2">
+      <Header />
       <About />
       <Projects />
     </div>
