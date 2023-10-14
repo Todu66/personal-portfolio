b77a5da0693c0f1c997773bbc55240e56cda5401
Giga Todua <89189706+Todu66@users.noreply.github.com>
2023-10-14 22:10:43 +0400

    adding rotate function to menu and styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,51 +3,68 @@ import Logo from "./assets/logo-d8b1678f-removebg-preview.png";
 import { menuOutline } from "ionicons/icons";
 import { closeOutline } from "ionicons/icons";
 import { IonIcon } from "@ionic/react";
+
+
 export const Header = () => {
   const [nav, setNav] = useState(false);
-  // filter: blur(5px) brightness(0.7);
+  const [rotateMenuIcon, setRotateMenuIcon] = useState(false);
+
+  const handleNavToggle = () => {
+    setNav(!nav);
+    setRotateMenuIcon(!rotateMenuIcon);
+  };
+
   return (
-    <div className="flex justify-between items-center ">
-      <div className="flex items-center w-16 h-16">
-        <img src={Logo} />
+    <div className="flex justify-between items-center">
+      <div className="flex items-center w-16 h-16 ">
+        <img src={Logo} alt="Logo" />
       </div>
-      {/* <div className=''>
-          <h2>Home</h2>
-          <h2>About</h2>
-          <h2>Projects</h2>
-          <h2>Contact</h2>
-        </div> */}
-      <div onClick={() => setNav(!nav)} className="cursor-pointer">
-        
-        <IonIcon className="text-[#64ffda] text-[45px]" icon={menuOutline} />
+
+      <div onClick={handleNavToggle} className="cursor-pointer z-20">
+      <IonIcon
+  className={`text-[#64ffda] text-[45px] rotate-icon ${
+    rotateMenuIcon ? "rotate-reverse" : ""
+  }`}
+  icon={nav ? closeOutline : menuOutline}
+/>
       </div>
-      {/* <i class="fa-solid fa-xmark"></i> */}
-      {/* --------------------------------------- */}
-      {/* Mobile Menu */}
-      {nav ? (
-        <div className=" filter-blur-brightness fixed w-full h-screen z-10 top-0 left-0"></div>
-      ) : (
-        ""
+
+      {nav && (
+        <div className="fixed w-full h-screen z-[2] top-0 left-0  bg-[#0a192f] bg-opacity-50"></div>
       )}
-      {/* Side drawer menu */}
+
       <div
         className={
           nav
-            ? "flex flex-col gap-16 fixed top-0 right-0 w-[130px] h-screen bg-[#112240] z-10 duration-300 p-2"
-            : "fixed top-0 right-[-200%] w-[300px] h-screen bg-[#112240] z-10 duration-300"
+            ? "flex flex-col justify-center gap-16 fixed top-0 right-0 w-[200px] h-screen bg-[#112240] z-10 duration-300 p-2 "
+            : "flex flex-col justify-center gap-16 fixed  top-0 right-[-200%] w-[300px] h-screen bg-[#112240]  duration-1000"
         }
       >
-        <div className="flex items-center ">
+        {/* <div className="flex justify-end">
+          <IonIcon
+            className={`text-[#64ffda] cursor-pointer text-[45px] ${
+              rotateMenuIcon ? "rotate-icon" : ""
+            }`}
+            icon={closeOutline}
+            onClick={handleNavToggle}
+          />
+        </div> */}
 
-            <IonIcon className="text-[#64ffda] cursor-pointer text-[45px]"  icon={closeOutline}  onClick={() => setNav(!nav)}/>   
-        </div>
-
-        <nav className="flex flex-col justify-center text-center gap-5 text-xl text-[#ccd6f6] ">
+        <nav className="flex flex-col mb-12 justify-center text-center gap-5 text-xl text-[#ccd6f6]">
           <h2 className="hover:text-[#64ffda] cursor-pointer">Home</h2>
           <h2 className="hover:text-[#64ffda] cursor-pointer">About</h2>
           <h2 className="hover:text-[#64ffda] cursor-pointer">Projects</h2>
           <h2 className="hover:text-[#64ffda] cursor-pointer">Contact</h2>
-          <button>Resume</button>
+          {/* {`px-4 py-2 border-solid border border-[#64ffda] rounded transition-all duration-250 ease-cubic hover:shadow-hover hover:translate-x-[-5px] hover:translate-y-[-5px]`} */}
+          <button
+            
+            className=" px-4 py-2 w-[80%] mx-auto border-solid border border-[#64ffda] text-[#64ffda] rounded transition-all duration-250 ease-cubic hover:shadow-hover hover:translate-x-[-5px] hover:translate-y-[-5px]"
+          >
+            Resume
+            <span className="ml-2">
+              <i className="fa-solid fa-download text-[18px]"></i>
+            </span>
+          </button>
         </nav>
       </div>
     </div>
